0b629fa86c93b90263b60f2dbc95ff2907738fb4
magiswebsite-dotcom <magiswebsite@gmail.com>
2026-05-12 19:32:29 -0400

    Update nav.js
    Forgot to include the Custom page on the navbar

--- nav.js
+++ nav.js
@@ -11,6 +11,7 @@ function loadNavbar() {
       <a href="about.html">About</a>
       <a href="products.html">Products</a>
       <a href="checkout.html">Cart (<span id="cart-count">0</span>)</a>
+      <a href="custom.html">Custom</a>
       <a href="contact.html">Contact</a>
     </nav>
   `;
